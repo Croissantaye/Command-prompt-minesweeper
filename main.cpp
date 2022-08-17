fd4ed431b602c4c4aef5aab0d382a5a79cfd64aa
Asante Azevedo <59629941+Croissantaye@users.noreply.github.com>
2022-08-17 12:30:54 -0700

    create board start

--- main.cpp
+++ main.cpp
@@ -4,12 +4,56 @@
 using namespace std;
 
 #define _MAXSIDES 25
+#define _BEGINNER 1
+#define _BEGINNER_SIDES 9
+#define _BEGINNER_MINES 4
+#define _INTERMEDIATE 2
+#define _INTERMEDIATE_SIDES 16
+#define _INTERMEDIATE_MINES 40
+#define _ADVANCED 3
+#define _ADVANCED_SIDES 24
+#define _ADVANCED_MINES 99
 
+int DIFFICULTY;
 int SIDE;
 int MINES;
 
+void chooseDifficulty();
+void createBoard();
+
 int main(){
-    cout << "hello world";
+    chooseDifficulty();
+    createBoard();
+}
+
+void chooseDifficulty(){
+    int level;
+
+    cout << "Enter the difficulty level" << endl;
+    cout << "Beginner:      1" << endl;
+    cout << "Intermediate:  2" << endl;
+    cout << "Advanced:      3" << endl;
+
+    cin >> level;
+    
+    switch (level)
+    {
+    case 1:
+        SIDE = _BEGINNER_SIDES;
+        MINES = _BEGINNER_MINES;
+        break;
+    case 2:
+        SIDE = _BEGINNER_SIDES;
+        MINES = _BEGINNER_MINES;
+        break;
+    case 3:
+        SIDE = _BEGINNER_SIDES;
+        MINES = _BEGINNER_MINES;
+        break;
+    
+    default:
+        break;
+    }
 }
 
 void printBoard(char myBoard[][_MAXSIDES]){
