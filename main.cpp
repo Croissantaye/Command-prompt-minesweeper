43cc6fd72ac2850143b3f0c49a292acf1b5780d9
Asante Azevedo <59629941+Croissantaye@users.noreply.github.com>
2022-08-19 17:16:22 -0700

    print board implemented

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 // #include <bits/stdc++.h>
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
@@ -17,13 +19,44 @@ using namespace std;
 int DIFFICULTY;
 int SIDE;
 int MINES;
+char** gameBoard;
 
 void chooseDifficulty();
 void createBoard();
+void fillBoard(char**);
+void printBoard(char**);
 
 int main(){
     chooseDifficulty();
     createBoard();
+    fillBoard(gameBoard);
+    printBoard(gameBoard);
+    
+}
+
+void createBoard(){
+    gameBoard = new char*[SIDE];
+
+    for(int i = 0; i < SIDE; i++){
+        gameBoard[i] = new char[SIDE];
+    }
+}
+void fillBoard(char** board){
+    srand(time(0));
+    int minesCount = 0;
+
+    for (int i = 0; i < SIDE; i++)
+    {
+        for (int j = 0; j < SIDE; j++){
+            if(rand() % SIDE == 0 && minesCount < SIDE){
+                board[i][j] = '*';
+                minesCount++;
+            }
+            else{
+                board[i][j] = '0';
+            }
+        }
+    }
 }
 
 void chooseDifficulty(){
@@ -56,21 +89,23 @@ void chooseDifficulty(){
     }
 }
 
-void printBoard(char myBoard[][_MAXSIDES]){
+void printBoard(char** myBoard){
     int i,j;
     printf("    ");
 
     for(i = 0; i < SIDE; i++){
         printf("%d ", i);
-        printf("\n\n");
     }
+    
+    printf("\n\n");
 
     for(i = 0; i < SIDE; i++){
-        printf("%d  ", i);
+        printf("%d   ", i);
 
         for(j = 0; j < SIDE; j++){
             printf("%c ", myBoard[i][j]);
-            printf("\n");
         }
+        printf("\n");
     }
+    printf("\n");
 }
